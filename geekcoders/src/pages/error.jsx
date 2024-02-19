fef43b408249a2c6e20f7e5fec80bb73ef17ca19
Pratyush Kamal <pratyushkamal88@gmail.com>
2024-02-19 12:02:54 +0530

    fixed error page

--- geekcoders/src/pages/error.jsx
+++ geekcoders/src/pages/error.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from 'react-router-dom';
 import errorImage from '../assets/error.png'; 
 import styles from './error.module.css'; 
 import Ellipses from "../components/Ellipses";
@@ -10,7 +10,8 @@ const Error = () => {
     <div className={styles.content}>
       <div className={styles.header}>TheFoodBarn</div>
       <img src={errorImage} alt="Error" className={styles.image} />
-      <button className={styles.button}>Retry</button>
+      {/* <button className={styles.button}>Go to HOME</button> */}
+      <Link to="/home" className={styles.button}>Go to HOME</Link>
     </div>
     <Ellipses/>
   </div>
